f66ee8f762e7967df1658be339ec6cd512919989
mirrrrrrrrrrrr <ilosrim@yahoo.com>
2025-03-12 12:25:10 +0500

    Avtomatik workflow yaratish

--- workflow.nix
+++ workflow.nix
@@ -1,29 +1,32 @@
-let
-  workflowContent = ''
-    name: CI
+{pkgs ? import <nixpkgs> {}}:
+pkgs.runCommand "generate-ci" {} ''
+  mkdir -p $out
+  cat > $out/ci.yml <<EOF
+  name: CI
 
-    on:
-      push:
-        branches:
-          - main
-      pull_request:
+  on:
+    push:
+      branches:
+        - main
+    pull_request:
 
-    jobs:
-      build:
-        runs-on: ubuntu-latest
+  jobs:
+    build:
+      runs-on: ubuntu-latest
 
-        steps:
-          - name: Kodni klonlash
-            uses: actions/checkout@v4
+      steps:
+        - name: Kodni klonlash
+          uses: actions/checkout@v4
 
-          - name: Nix-ni o'rnatish
-            uses: cachix/install-nix-action@v27
+        - name: Nix-ni o'rnatish
+          uses: cachix/install-nix-action@v27
 
-          - name: Nix develop orqali ishlash
-            run: |
-              nix develop --command bash -c "
-              echo 'Salom, Nix workflow ishlayapti!'
-              "
-  '';
-in
-  builtins.toFile "ci.yml" workflowContent
+        - name: Nix develop orqali ishlash
+          run: |
+            nix develop --command bash -c "
+            echo 'Salom, Nix workflow ishlayapti!'
+            "
+  EOF
+  cp $out/ci.yml ./ci.yml
+  chmod +w ./ci.yml  # Faylga yozish huquqini qo‘shamiz
+''
